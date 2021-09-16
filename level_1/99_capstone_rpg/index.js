6bf65001e7873032b63a6285f889758cea26e70d
Geoff Davis <Davis.Geoff2@gmail.com>
2021-09-15 22:48:26 -0700

    99 Capstone RPG: added attack method and various loops

--- level_1/99_capstone_rpg/index.js
+++ level_1/99_capstone_rpg/index.js
@@ -1,32 +1,118 @@
+const readlineSync = require('readline-sync');
+
 console.log('\033c');
 
-class Character {
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+class Avatar {
     constructor() {
-        this.name = 'Geoff';
         this.hp = 100;
+        this.minAttackValue = 10;
+        this.maxAttackValue = 30;
     }
     adjustHp(points) {
         this.hp += points;
     }
+    attacks(attacked) {
+        const damage = Math.min(attacked.hp, randomInteger(this.minAttackValue, this.maxAttackValue));
+        attacked.adjustHp(-damage);
+        console.log(`${this.constructor.name} inflicts ${damage} damage to ${attacked.constructor.name} which now has ${attacked.hp} HP left.`);
+        if (attacked.hp <= 0) console.log();
+    }
 }
 
-class Player extends Character {
-    constructor() {
+class Player extends Avatar {
+    constructor(name) {
         super();
-        this.inventory = [];        
+        this.name = name;
+        this.inventory = [];
+        // Overwrite basic Avatar Min/Max Attack Value
+        // this.minAttackValue = 50;
+        // this.maxAttackValue = 70;
+
+        // Overwrite starting HP
+        // this.hp = 200;
     }
     addToInventory(theItem) {
         this.inventory.push(theItem);
     }
+    takeFromInventory(theItem) {
+        this.inventory.splice(this.inventory.indexOf(theItem), 1);
+    }
+    enemyAppears(chance, enemyList) {
+        if (chance > Math.random()) return new enemyList[Math.floor(Math.random() * enemyList.length)];
+        return null;
+    }
 }
 
-let me = new Player();
+class Dragon extends Avatar {
+    constructor() {
+        super();
+        // Overwrite basic Avatar Min/Max Attack Value
+        // this.minAttackValue = 50;
+        // this.maxAttackValue = 70;
+
+        // Overwrite starting HP
+        // this.hp = 200;
+    }
+}
+
+class Bugbear extends Avatar {
+    constructor() {
+        super();
+        // Overwrite basic Avatar Min/Max Attack Value
+        // this.minAttackValue = 50;
+        // this.maxAttackValue = 70;
+
+        // Overwrite starting HP
+        // this.hp = 200;
+    }
+
+}
+
+class Cyclops extends Avatar {
+    constructor() {
+        super();
+        // Overwrite basic Avatar Min/Max Attack Value
+        // this.minAttackValue = 50;
+        // this.maxAttackValue = 70;
+
+        // Overwrite starting HP
+        // this.hp = 200;
+    }
+}
+
+function battle(thePlayer, theEnemy) {
+    console.log(`Let the BATTLE begin!!!\n`);
+    do {
+        thePlayer.attacks(theEnemy);
+        if (theEnemy.hp <= 0) return `You killed the ${theEnemy.constructor.name}!`;
+        theEnemy.attacks(thePlayer);
+        if (thePlayer.hp <= 0) return `The ${theEnemy.constructor.name} killed you!`;
+        console.log();
+        // userResponse = readlineSync.question("Press Enter for the next round...");
+    } while (true);
+};
+
+const player1 = new Player('Geoff');
+const enemyList = [Dragon, Bugbear, Cyclops];
+
+// const newEnemy = new Dragon();
 
 
-console.log(me);
-me.adjustHp(4);
-console.log(me);
-me.adjustHp(-14);
-console.log(me);
-me.addToInventory('Spear');
-console.log(me);
+
+
+do {
+    userResponse = readlineSync.question('Hit "W" to Walk: ');
+    if (userResponse = 'W') {
+        const newEnemy = player1.enemyAppears(1 / 4, enemyList);
+        if (newEnemy) {
+            console.log(`A ${newEnemy.constructor.name} has appeared!!!`);
+            console.log(battle(player1, newEnemy));
+        }
+        else console.log('Whew!!! You walked without encountering any enemies.');
+    }
+    console.log();
+} while (true);
