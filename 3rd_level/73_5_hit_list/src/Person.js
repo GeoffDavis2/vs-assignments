2e1484631fd7f28fdb78548b4db83c416284101f
Geoff Davis <Davis.Geoff2@gmail.com>
2021-10-20 21:16:06 -0700

    L3, 73_5_hit_list: Added Extras Added Intentional Delay and "Loading..." Added Big Red "X" when you click on the person.

--- 3rd_level/73_5_hit_list/src/Person.js
+++ 3rd_level/73_5_hit_list/src/Person.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 
 class Person extends Component {
 
-    render = () => <div className='person'>
-        <img src={this.props.person.image} alt='' className='person-image'/>
+    render = () => <div
+        className={this.props.person.takenCareOf ? 'person case-closed' : 'person'}
+        onClick={() => this.props.handleClick(this.props.person.id)}
+    >
+        <img src={this.props.person.image} alt='' className='person-image' />
         <h3 className='name'>{this.props.person.name}</h3>
     </div>
 
--- 3rd_level/73_5_hit_list/src/index.js
+++ 3rd_level/73_5_hit_list/src/index.js
@@ -7,21 +7,38 @@ import './style.css';
 const axios = require("axios");
 
 class RootComponent extends Component {
-    state = { persons: [] };
+    state = {
+        isLoading: true,
+        persons: []
+    };
 
     componentDidMount() {
-        axios
-            .get('https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json')
-            .then(res => {
-                const persons = res.data;
-                this.setState({ persons });
-            })
-            .catch(err => alert(err));
+        setTimeout(() => {
+            this.setState({ isLoading: false })
+            axios
+                .get('https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json')
+                .then(res => {
+                    const persons = res.data;
+                    persons.forEach((person, i) => {
+                        person.id = i;
+                        person.takenCareOf = false;
+                    });
+                    this.setState({ persons });
+                })
+                .catch(err => alert(err))
+        }, 1000);
+    }
+
+    handleClick = (id) => {
+        const tempArr = JSON.parse(JSON.stringify(this.state.persons));
+        tempArr[id].takenCareOf = !tempArr[id].takenCareOf;
+        this.setState({ persons: tempArr });
     }
 
     render = () =>
-        <>            
-            {this.state.persons.map((person, i) => <Person key={i} person={person} />)}
+        <>
+            {this.state.isLoading ? <h1 id='loading'>Loading...</h1> : null}
+            {this.state.persons.map((person, i) => <Person key={i} person={person} handleClick={this.handleClick} />)}
         </>
 
 }
