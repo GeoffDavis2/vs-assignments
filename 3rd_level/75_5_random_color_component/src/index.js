cf3c465cb23543c60ac396f7e705901263c47963
Geoff Davis <Davis.Geoff2@gmail.com>
2021-10-22 10:27:20 -0700

    L3, 75_5_random_color_component: async + other imrpovements - Fixed url to use tick mark (`) and ${variables} - Larger Circle Spacing - Added  handleIntervals tempArr.push which uses async/await - Kept the original  handleIntervals tempArr.push which uses .then but commented it out - Added intentional delay of 1 second and modified render to use ternary

--- 3rd_level/75_5_random_color_component/src/index.js
+++ 3rd_level/75_5_random_color_component/src/index.js
@@ -3,34 +3,40 @@ import ReactDOM from 'react-dom';
 import './style.css';
 const axios = require("axios");
 
-
-
 class RootComponent extends Component {
-    constructor() {
-        super();
+    state = {
+        isLoading: true,
+        circles: []
+    };
 
-        this.state = {
-            circles: [
-                { size: 100, color: 'blue' }
-            ]
-        };
-
-        this.handleIntervals();
-
+    async componentDidMount() {
+        const resp = await this.getRandomColor();
+        await this.setState({ circles: [{ size: 50, color: `#${resp.data.new_color}` }] });
+        setTimeout(() => {
+            this.setState({ isLoading: false })
+            this.handleIntervals();
+        }, 1000);
     }
 
-    handleIntervals() {
+    async handleIntervals() {
         this.int = setInterval(() => {
             let tempArr = JSON.parse(JSON.stringify(this.state.circles));
-            
-            tempArr.forEach(circle => {
-                circle.size += 10;
-            })
 
-            if (tempArr[tempArr.length - 1].size >= 100)
-                this.getRandomColor()
-                    .then(resp => tempArr
-                        .push({ size: 50, color: '#' + resp.data.new_color }))
+            tempArr.forEach(circle => circle.size += 10);
+
+            // Using async / await, not sure it's any cleaner or easier to understand...?
+            if (tempArr[tempArr.length - 1].size >= 200)
+                (async () => {
+                    let resp = await this.getRandomColor();
+                    await tempArr.push({ size: 50, color: `#${resp.data.new_color}` });
+                })()
+
+            // or Using Promise / then
+            // Is this better than async/awiat, not sure...?
+            // if (tempArr[tempArr.length - 1].size >= 200)
+            //     this.getRandomColor()
+            //         .then(resp => tempArr
+            //             .push({ size: 50, color: `#${resp.data.new_color}` }))
 
             if (tempArr[0].size >= 3000) tempArr.shift()
 
@@ -38,21 +44,20 @@ class RootComponent extends Component {
         }, 75)
     }
 
-    getRandomColor = () => axios.get('https://www.colr.org/json/color/random?timestamp=' + new Date().getTime());
+    getRandomColor = () => axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`);
 
     render = () =>
-        <>
-            {this.state.circles.map((circle, i) => <div
+        <>{this.state.isLoading
+            ? <h1 id='loading'>Loading...</h1>
+            : this.state.circles.map((circle, i) => <div
                 key={i}
                 className='circle-css'
-                style={
-                    {
-                        width: `${circle.size}px`,
-                        backgroundColor: `${circle.color}`
-                    }
-                }
-            ></div>)}
-        </>
+                style={{
+                    width: `${circle.size}px`,
+                    backgroundColor: `${circle.color}`
+                }}
+            ></div>)
+        }</>
 }
 
 ReactDOM.render(<RootComponent />, document.getElementById('root-container'));
