a15c19d2ee2783f211086632a710b32a9f54447d
Geoff Davis <Davis.Geoff2@gmail.com>
2021-11-22 21:29:57 -0800

    338_thing_finder: Finished

---
+++ 5th_level/338_thing_finder/main.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express();
+const { v4: uuid } = require("uuid");
+
+
+let { docs } = require("./documents");
+docs = docs.map(doc => ({ _id: uuid(), ...doc }));
+
+console.log("\033c");
+app.use(express.json());
+
+
+app.get("/", (_, res) => res.status(200).json(docs));
+
+app.get("/:id", (req, res) => res
+    .status(200)
+    .json(docs.find(y => y._id === req.params.id))
+);
+
+app.get("/sea/rch", (req, res) => {
+    qryObj = req.query;
+    const yeild = docs.filter(doc => {
+        let found = true;
+        for (key in qryObj) found = found && (doc[key] == qryObj[key]);
+        return found;
+    })
+    res.status(200).json(yeild);
+});
+
+app.listen(6789, () => {
+    console.log("Server is running on port 6789");
+});
