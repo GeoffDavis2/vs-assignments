436860141b606ba3c01cd110f6c31449215c976b
Geoff Davis <Davis.Geoff2@gmail.com>
2021-11-22 17:52:51 -0800

    335_bounty_hunter_pt1: Finished again???

--- 5th_level/335_bounty_hunter_pt1/main.js
+++ 5th_level/335_bounty_hunter_pt1/main.js
@@ -1,2 +1,86 @@
-console.log('hello');
-console.log('hello');
+const express = require('express')
+const app = express();
+const { v4: uuid } = require('uuid');
+
+app.use(express.json());
+
+const bounties = [
+    {
+        FirstName: 'Geoff',
+        LastName: 'Davis',
+        Living: true,
+        BountyAmount: 50,
+        Type: 'Jedi'
+    },
+    {
+        FirstName: 'John',
+        LastName: 'Doe',
+        Living: true,
+        BountyAmount: 500,
+        Type: 'Sith'
+    },
+    {
+        FirstName: 'Spike',
+        LastName: 'Jones',
+        Living: true,
+        BountyAmount: 500,
+        Type: 'Jedi'
+    },
+    {
+        FirstName: 'Luke',
+        LastName: 'Skywalker',
+        Living: false,
+        BountyAmount: 697,
+        Type: 'Jedi'
+    },
+    {
+        FirstName: 'Darth',
+        LastName: 'Vader',
+        Living: false,
+        BountyAmount: 0,
+        Type: 'Sith'
+    },
+    {
+        FirstName: 'Leia',
+        LastName: 'Organa',
+        Living: true,
+        BountyAmount: 550,
+        Type: 'Jedi'
+    },
+    {
+        FirstName: 'Obi-Wan',
+        LastName: 'Kenobi',
+        Living: false,
+        BountyAmount: 5000,
+        Type: 'Jedi'
+    },
+    {
+        FirstName: 'Biggs',
+        LastName: 'Darklighter',
+        Living: true,
+        BountyAmount: 5010,
+        Type: 'Sith'
+    }
+
+]
+bounties.map(bounty => bounty._id = uuid());
+
+app.get('/bounties', (req, res) => {
+    res.json(bounties);
+})
+
+app.post('/bounties', (req, res) => {
+    const bounty = req.body;
+    bounty._id = uuid();
+    bounties.push(bounty);
+    res
+        .status(200)
+        .json({
+            message: 'Post successfull.',
+            data: bounties
+        });
+})
+
+app.listen(6789, () => {
+    console.log('Server is running on port 6789');
+});
